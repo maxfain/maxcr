7db2aa7deed7cbf96eb8ea0204870421d65e1e83
Hans <hansl@agentmail.com>
2026-03-16 21:19:48 -0400

    Add GitHub social link (maxfain)

--- src/constants.ts
+++ src/constants.ts
@@ -2,6 +2,7 @@ import type { Props } from "astro";
 import IconMail from "@/assets/icons/IconMail.svg";
 import IconBrandX from "@/assets/icons/IconBrandX.svg";
 import IconLinkedin from "@/assets/icons/IconLinkedin.svg";
+import IconGitHub from "@/assets/icons/IconGitHub.svg";
 import IconWhatsapp from "@/assets/icons/IconWhatsapp.svg";
 import IconFacebook from "@/assets/icons/IconFacebook.svg";
 import IconTelegram from "@/assets/icons/IconTelegram.svg";
@@ -27,6 +28,12 @@ export const SOCIALS: Social[] = [
     href: "https://x.com/maxcr",
     linkTitle: `${SITE.title} on X`,
     icon: IconBrandX,
+  },
+  {
+    name: "GitHub",
+    href: "https://github.com/maxfain",
+    linkTitle: `${SITE.title} on GitHub`,
+    icon: IconGitHub,
   },
 ] as const;
 
